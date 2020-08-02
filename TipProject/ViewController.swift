20631eee7be3fd8dc8b380d7d7ea19c4f1ab0583
hamaro01 <Katherine.helena01@gmail.com>
2020-08-01 21:19:27 -0400

    Connect label, text, object to code in swift

--- TipProject/ViewController.swift
+++ TipProject/ViewController.swift
@@ -10,12 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    @IBOutlet weak var tipText: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalText: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("Hello")
+        view.backgroundColor = .blue
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+    
+    
 
 
 }
